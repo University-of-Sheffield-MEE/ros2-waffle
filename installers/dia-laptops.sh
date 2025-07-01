4232e734933aeb6dc21aa885137cd543069a0278
Tom Howard <t.howard@sheffield.ac.uk>
2025-07-01 14:04:46 +0100

    Don't use experimental stuff

--- installers/dia-laptops.sh
+++ installers/dia-laptops.sh
@@ -90,7 +90,6 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
 sudo apt update
 export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
 sudo apt install -y nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
